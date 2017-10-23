eaf62f05234744181f537e61381a914a9037c18b
Matt Smithies <matt.smithies@theassetworks.com>
2017-10-23 14:03:33 +0100

    Using auto height for the dialog view

--- src/screens/Balances.js
+++ src/screens/Balances.js
@@ -41,12 +41,12 @@ export default class Balances extends Component {
           <Text style={styles.buttonText}>Add new debt</Text>
         </TouchableHighlight>
         <PopupDialog
+          height={null}
           dialogTitle={<DialogTitle title="Create Debt" />}
           ref={(createDebtDialog) => { this.createDebtDialog = createDebtDialog;}}
           dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' })}>
           <AddDebt />
         </PopupDialog>
-
       </View>
     );
   }
