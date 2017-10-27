8cfea60ef9a703c81e13458b787d28543971b033
Matt Smithies <matt.smithies@theassetworks.com>
2017-10-27 06:47:39 +0100

    Dialog - Balance History, when user is selected on balance view

--- src/screens/Balances.js
+++ src/screens/Balances.js
@@ -10,10 +10,27 @@ import {
   Image
 } from 'react-native';
 
-import AddDebt from '../components/dialogs/AddDebt';
+import AddDebt from '../components/dialogs/addDebt/AddDebt';
+import DebtHistory from '../components/dialogs/debtHistory/DebtHistory';
 import BalanceList from '../components/listviews/balance/BalanceListview';
 
 import styles from './styles';
+
+//this will be a key (history) inside of Balances
+const HISTORY = [
+  {time: 1508510707000, amount: "100", state: "dr", memo: "test1", currency: "$USD"},
+  {time: 1506510707000, amount: "100", state: "cr", memo: "test2", currency: "$USD"},
+  {time: 1518510707000, amount: "100", state: "dr", memo: "test2", currency: "$USD"},
+  {time: 1208510707000, amount: "100", state: "dr", memo: "test1", currency: "£GBP"},
+  {time: 1508410707000, amount: "100", state: "cr", memo: "test1", currency: "$USD"},
+  {time: 108510707000, amount: "100", state: "dr", memo: "test13", currency: "$USD"},
+  {time: 1508510707000, amount: "100", state: "dr", memo: "test1", currency: "$USD"},
+  {time: 1506510707000, amount: "100", state: "cr", memo: "test1", currency: "$USD"},
+  {time: 1518510707000, amount: "100", state: "dr", memo: "test1", currency: "£GBP"},
+  {time: 1208510707000, amount: "100", state: "dr", memo: "test1", currency: "$USD"},
+  {time: 1508410707000, amount: "100", state: "cr", memo: "test1", currency: "$USD"},
+  {time: 108510707000, amount: "100", state: "dr", memo: "test1", currency: "$USD"}
+]
 
 //amount
 //name
@@ -24,12 +41,12 @@ import styles from './styles';
 //last_transaction
 //total_debts
 const BALANCE_MOCK_DATA = [
-  {id: 1, amount: "300.78", name: "Tim", state: "dr", currency: "USD", curr_sym: "$", last: 1508510707000, total_debts: "2"},
-  {id: 2, amount: "66.21", name: "Matt", state: "dr", currency: "GBP", curr_sym: "£", last: 1508673607000, total_debts: "7"},
-  {id: 3, amount: "9.00", name: "Derek", state: "cr", currency: "USD", curr_sym: "$", last: 1508486827000, total_debts: "1"}]
+  {id: 1, amount: "300.78", name: "Tim", history: HISTORY, state: "dr", currency: "USD", curr_sym: "$", last: 1508510707000, total_debts: "2"},
+  {id: 2, amount: "66.21", name: "Matt", history: HISTORY, state: "dr", currency: "GBP", curr_sym: "£", last: 1508673607000, total_debts: "7"},
+  {id: 3, amount: "9.00", name: "Derek", history: HISTORY, state: "cr", currency: "USD", curr_sym: "$", last: 1508486827000, total_debts: "1"}]
 
 export default class Balances extends Component {
-    static navigationOptions = {
+  static navigationOptions = {
     title: 'Balances of Users',
     tabBarLabel: 'Balances'
   }
@@ -42,10 +59,24 @@ export default class Balances extends Component {
     };
 
     this.showCreateDebtDialog = this.showCreateDebtDialog.bind(this);
+    this.showDebtHistoryDialog = this.showDebtHistoryDialog.bind(this);
+  }
+
+  showDebtHistoryDialog() {
+    this.createHistoryDialog.show();
   }
 
   showCreateDebtDialog() {
     this.createDebtDialog.show();
+  }
+
+  displayDebtHistory = (id) => {
+    const user = BALANCE_MOCK_DATA.filter(function (elem) {
+      return elem.id == id
+    })[0];
+
+    this.debtHistory.attachUser(user)
+    this.showDebtHistoryDialog()
   }
 
   render() {
@@ -53,6 +84,7 @@ export default class Balances extends Component {
     return (
       <View style={styles.container}>
         <BalanceList
+          displayDebt={this.displayDebtHistory}
           data={BALANCE_MOCK_DATA}/>
         <PopupDialog
           height={null}
@@ -60,6 +92,14 @@ export default class Balances extends Component {
           ref={(createDebtDialog) => { this.createDebtDialog = createDebtDialog;}}
           dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' })}>
           <AddDebt />
+        </PopupDialog>
+        <PopupDialog
+          height={0.7}
+          dialogTitle={<DialogTitle title="Debt History" />}
+          ref={(createHistoryDialog) => { this.createHistoryDialog = createHistoryDialog;}}
+          dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' })}>
+          <DebtHistory
+            ref={component => this.debtHistory = component}/>
         </PopupDialog>
         <ActionButton buttonColor="rgba(231,76,60,1)">
           <ActionButton.Item buttonColor='#26c6da' title="My profile" onPress={() => console.log("My Profile")}>
