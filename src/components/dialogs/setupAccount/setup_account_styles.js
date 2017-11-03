c110797b90e871de0d9718140db787c1b15ca0b7
Matt Smithies <matt.smithies@theassetworks.com>
2017-11-03 14:47:38 +0000

    Setup dialog for creating new private key with 12 words, key generation required

---
+++ src/components/dialogs/setupAccount/setup_account_styles.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+    StyleSheet
+} from 'react-native';
+
+const setup_account = StyleSheet.create({
+  dialog_margins: {
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 12,
+    padding: 2,
+    marginBottom: 10
+  },
+  dialog_text: {
+    alignSelf: 'center',
+    fontSize: 20,
+    padding: 10
+  },
+  section_title: {
+    alignSelf: 'center',
+    fontSize: 20,
+    padding: 10,
+    fontWeight: 'bold',
+  },
+  record_words: {
+    alignSelf: 'center',
+    fontSize: 18,
+    padding: 10,
+    marginLeft: 25,
+    marginRight: 25,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'red',
+    marginBottom: 20
+  },
+  dialog_button: {
+    alignSelf: 'stretch',
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 10,
+    borderRadius: 5,
+    height: 50,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    borderColor: 'grey',
+    borderWidth: 1
+  },
+  dialog_text: {
+    alignSelf: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#03A9F4'
+  },
+});
+
+export default setup_account;
