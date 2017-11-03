7333315423f7b30416826ef3259b9b6b7b7ca2e6
Matt Smithies <matt.smithies@theassetworks.com>
2017-11-03 14:50:58 +0000

    updated show account padding of dialog view, styles for button

--- src/components/dialogs/showAccount/show_account_styles.js
+++ src/components/dialogs/showAccount/show_account_styles.js
@@ -5,11 +5,7 @@ import {
 
 const show_account = StyleSheet.create({
   dialog_margins: {
-    marginLeft: 20,
-    marginRight: 20,
     padding: 12,
-    padding: 2,
-    marginTop: 10,
     marginBottom: 10
   },
   dialog_text: {
@@ -22,7 +18,27 @@ const show_account = StyleSheet.create({
     fontSize: 20,
     padding: 10,
     fontWeight: 'bold',
-  }
+  },
+  dialog_button: {
+    alignSelf: 'stretch',
+    marginTop: 10,
+    marginBottom: 5,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 10,
+    borderRadius: 5,
+    height: 50,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    borderColor: 'grey',
+    borderWidth: 1
+  },
+  dialog_text: {
+    alignSelf: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#03A9F4'
+  },
 });
 
 export default show_account;
