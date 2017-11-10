30bc98ae8a14026b27408ec74775019c9ffec5bd
Nate Ferrero <nateferrero@gmail.com>
2017-11-10 15:23:15 -0700

    Fix tabContainerStyle prop on <Tabs>

--- packages/theme/account.ts
+++ packages/theme/account.ts
@@ -1,14 +1,9 @@
 import { StyleSheet } from 'react-native'
 
 import { xxl } from 'theme/include/spacing'
-import { secondary } from 'theme/include/colors'
 
 export default StyleSheet.create({
   tabs: {
     paddingTop: xxl
-  },
-
-  secondaryTabs: {
-    backgroundColor: secondary
   }
 })
