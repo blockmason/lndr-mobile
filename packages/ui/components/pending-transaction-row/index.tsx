dc48eeaa9c689362ca05444f5d5b7fcf1efe5271
Matt Smithies <matt.smithies@theassetworks.com>
2017-11-17 09:36:07 +0000

    Single child for pending row for highlight touchable

--- packages/ui/components/pending-transaction-row/index.tsx
+++ packages/ui/components/pending-transaction-row/index.tsx
@@ -19,27 +19,29 @@ export default class PendingTransactionRow extends Component<Props> {
 
     return (
       <TouchableHighlight onPress={onPress} underlayColor={lightGray} activeOpacity={0.5} key={pendingTransaction.hash}>
-        <View style={style.listItem}>
-          <Text style={style.title}>Creditor</Text>
-        </View>
-        <View style={style.listItem}>
-          <Text style={style.fact}>@{pendingTransaction.creditRecord.creditorAddress.substr(0, 8)}</Text>
-          <Text style={style.address}>{pendingTransaction.creditRecord.creditorAddress}</Text>
-        </View>
-        <View style={style.listItem}>
-          <Text style={style.title}>Debtor (owes the debt)</Text>
-        </View>
-        <View style={style.listItem}>
-          <Text style={style.fact}>@{pendingTransaction.creditRecord.debtorAddress.substr(0, 8)}</Text>
-          <Text style={style.address}>{pendingTransaction.creditRecord.debtorAddress}</Text>
-        </View>
-        <View style={style.listItem}>
-          <Text style={style.title}>Memo</Text>
-          <Text style={style.text}>{pendingTransaction.creditRecord.memo}</Text>
-        </View>
-        <View style={style.listItem}>
-          <Text style={style.title}>Amount</Text>
-          <Text style={style.text}>{pendingTransaction.creditRecord.amount} US cents</Text>
+        <View>
+          <View style={style.listItem}>
+            <Text style={style.title}>Creditor</Text>
+          </View>
+          <View style={style.listItem}>
+            <Text style={style.fact}>@{pendingTransaction.creditRecord.creditorAddress.substr(0, 8)}</Text>
+            <Text style={style.address}>{pendingTransaction.creditRecord.creditorAddress}</Text>
+          </View>
+          <View style={style.listItem}>
+            <Text style={style.title}>Debtor (owes the debt)</Text>
+          </View>
+          <View style={style.listItem}>
+            <Text style={style.fact}>@{pendingTransaction.creditRecord.debtorAddress.substr(0, 8)}</Text>
+            <Text style={style.address}>{pendingTransaction.creditRecord.debtorAddress}</Text>
+          </View>
+          <View style={style.listItem}>
+            <Text style={style.title}>Memo</Text>
+            <Text style={style.text}>{pendingTransaction.creditRecord.memo}</Text>
+          </View>
+          <View style={style.listItem}>
+            <Text style={style.title}>Amount</Text>
+            <Text style={style.text}>{pendingTransaction.creditRecord.amount} US cents</Text>
+          </View>
         </View>
       </TouchableHighlight>
     )
