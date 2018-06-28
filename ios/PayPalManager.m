6624cac865e099a42c9dee16ddfd93ea42ea2888
Ethan Arutunian <ethan@arutunian.com>
2018-06-28 10:48:15 -0700

    moved iOS PayPal config to plist

--- ios/PayPalManager.m
+++ ios/PayPalManager.m
@@ -10,10 +10,6 @@
 #import "AppDelegate.h"
 #import <React/RCTLog.h>
 
-// TODO: move these outta here
-const NSString *PAYPAL_CLIENT_ID_PROD = @"AXOXl_tY00hi203wiplGZSDfP1T0W463wnDqfd4jFh3yG1ABkbPrrNCex1F8YXEROmqcVnSzVNTF80D2";
-const NSString *PAYPAL_CLIENT_ID_DEV = @"AQabZLoBTVKngs5UNiUWKk4CCjzh8EvPGoqz07nzzWPjYsVypvPRN9vVafll8Op-r3CKMHoBeygbf0pW";
-
 @interface PayPalManager ()
 @property (nonatomic, strong, readwrite) PayPalConfiguration *payPalConfiguration;
 @property (nonatomic, strong) RCTPromiseResolveBlock resolver;
@@ -25,8 +21,20 @@ const NSString *PAYPAL_CLIENT_ID_DEV = @"AQabZLoBTVKngs5UNiUWKk4CCjzh8EvPGoqz07n
 RCT_EXPORT_MODULE();
 
 + (void)setup {
-  [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentProduction: PAYPAL_CLIENT_ID_PROD,
-                                                         PayPalEnvironmentSandbox: PAYPAL_CLIENT_ID_DEV}];
+  NSString *configPath = [[NSBundle mainBundle] pathForResource:@"PayPalConfig" ofType:@"plist"];
+  if (!configPath) {
+    NSLog(@"ERROR: missing PayPalConfig");
+    return;
+  }
+  NSDictionary *theDict = [NSDictionary dictionaryWithContentsOfFile:configPath];
+  NSString *sandboxId = [theDict objectForKey:@"sandboxId"];
+  NSString *prodId = [theDict objectForKey:@"productionId"];
+  if ( (!sandboxId) || (!prodId) ) {
+    NSLog(@"ERROR: PayPalConfig missing keys");
+    return;
+  }
+  [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentProduction: prodId
+                                                         ,PayPalEnvironmentSandbox: sandboxId}];
 }
 
 RCT_EXPORT_METHOD(initPayPal) {
