15003744572317939bd69f9f981905a7a90b5373
Will Galebach <willbach@gmail.com>
2018-09-07 17:30:46 -0500

    Fixed styling on balance row component

--- packages/theme/account.ts
+++ packages/theme/account.ts
@@ -64,7 +64,7 @@ export default StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '80%',
+    width: '90%',
     borderColor: softGray,
     borderBottomWidth: 1,
     borderTopWidth: 1,
@@ -379,6 +379,15 @@ export default StyleSheet.create({
   },
   greenAmount: {
     color: moneyGreen
+  },
+  currencyBalance: {
+    flexDirection: 'row'
+  },
+  currencySymbolBalance: {
+    ...large,
+    textAlign: 'left',
+    color: black,
+    marginRight: 5
   },
   currencySymbol: {
     ...large,
