0931a0401730e97e2e09ef5be1b1df16e2414e1b
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-09-11 14:12:30 +0100

    Feature: Reorder settle up summary

---
+++ packages/lndr/settlements.ts
@@ -0,0 +1,14 @@
+export default [
+    {
+        settlementOption: 'Paypal',
+        settlementType: 'paypal'
+    },
+    {
+        settlementOption: 'Ethereum',
+        settlementType: 'eth'
+    },
+    {
+        settlementOption: 'Record a settlement',
+        settlementType: 'record'
+    }
+]
