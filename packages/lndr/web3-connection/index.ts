d054ed9bf64f61553946a4b6226be1c3ab515396
Devin Canterberry <devin@canterberry.cc>
2019-03-19 15:20:22 -0700

    🔑 Update Infura URLs
    Infura has deprecated the authentication strategy used in these URLs,
    and will completely remove support for them on March 27, 2019.
    
    This commit updates the URLs to use their new authentication strategy,
    which corresponds to a designated LNDR project within Infura.

--- packages/lndr/web3-connection/index.ts
+++ packages/lndr/web3-connection/index.ts
@@ -1,7 +1,7 @@
 import Web3 from 'web3'
 import { Platform } from 'react-native'
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/EoLr1OVfUMDqq3N2KaKA'))
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/cebe64ecf2584917a71e35787b2b4ef6'))
 // const web3 = Platform.OS === 'ios' ? new Web3(new Web3.providers.HttpProvider('http://localhost:8545')) : new Web3(new Web3.providers.HttpProvider('http://10.0.2.2:8545'))
 
 export default web3
